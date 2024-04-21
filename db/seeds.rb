1aa271218aab4e53f3530a544aea1128dbddac49
Dante Donaldson <ddonaldsonprime@gmail.com>
2024-04-21 15:35:12 -0500

    completed rails store-app problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create!(name: "Lays", price: 4.99, description: "Lay's Potato Chips are a popular snack known for their crispy texture and savory flavor")
+Product.create!(name: "Doritos", price: 4.25, description: "Doritos are a brand of flavored tortilla chips known for their bold and zesty taste")
+Product.create!(name: "Cheetos", price: 3.79, description: "Cheetos are a type of cheese-flavored puffed cornmeal snack known for their light and crunchy texture")
